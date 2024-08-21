b162fa8a15907fe701f77eef343461bf92af80b8
Abdul Zedan <42303579+zedan10@users.noreply.github.com>
2024-08-21 13:12:51 -0400

    activating venv in load_python

--- scripts/load_python_env.sh
+++ scripts/load_python_env.sh
@@ -5,3 +5,6 @@ python3 -m venv .venv
 
 echo 'Installing dependencies from "requirements.txt" into virtual environment (in quiet mode)...'
 .venv/bin/python -m pip --quiet --disable-pip-version-check install -r ./scripts/requirements.txt
+
+echo 'Activating the virtual environment...'
+. .venv/bin/activate
